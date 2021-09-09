a58f78615ea4f09878bf9fcecb6d7fff6c43bd73
Igor Czajkowski <igor.czajkowski@j-labs.pl>
2021-09-09 15:19:57 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>CreatePost</h1>
+      <PostCreate></PostCreate>
+      <hr />
+      <h1>Post List</h1>
+      <PostList />
+    </div>
+  );
+};
+
+export default App;
