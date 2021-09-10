8514e4cda9a78181a9b3feda836574dd4638c286
Igor Czajkowski <igor.czajkowski@j-labs.pl>
2021-09-10 14:50:30 +0200

    Moderate comments

--- comments/index.js
+++ comments/index.js
@@ -38,13 +38,28 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  // const { type, data } = req.body;
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
 
-  // if(type === "CommentModerated") {
-  //   const { postId, id, status } = data;
-  //   const comments = commentsByPostId[postId]
-  // }
+  if (type === "CommentModerated") {
+    const { postId, id, status } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content: comment.content,
+      },
+    });
+  }
 
   res.send({});
 });
--- query/index.js
+++ query/index.js
@@ -31,6 +31,18 @@ app.post("/events", (req, res) => {
     }
   }
 
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
   res.send({});
 });
 
